bfabd8ab2a7d9d6d9db224c9917d4b332f1d2d84
priyanair9 <priya.nair@students.mq.edu.au>
2021-08-10 23:32:39 +1000

    updated class files

--- src/Grid.java
+++ src/Grid.java
@@ -1,9 +1,24 @@
-public class Grid {
-    //instance variables
+import java.awt.*;
 
-    //constructors
-public Grid() {
-    
-}
-    //methods
+class Grid {
+    //fields
+    Cell[][] cells = new Cell[20][20];
+
+    // constructor
+    public Grid(){
+        for(int i = 0; i < cells.length; i++){
+            for(int j = 0; j < cells[i].length; j++){
+                cells[i][j] = new Cell(10+35*i,10+35*j);
+            }
+        }
+    }
+
+    // methods
+    public void paint(Graphics g, Point mousePos){
+        for(int i = 0; i < cells.length; i++){
+            for(int j = 0; j < cells[i].length; j++){
+                cells[i][j].paint(g, mousePos);
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,84 +1,40 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-import java.awt.*;
 import javax.swing.*;
+import java.awt.*;
 
-public class Main extends JFrame{
-    public static void main(String[] args) throws Exception {
-        Main window = new Main();
-        // window.run();
-    }
-    public class Canvas extends JPanel {
+class Main extends JFrame {
+    
+    class App extends JPanel {
+        
         Grid grid;
-        public Canvas() {
+
+        public App() {
             setPreferredSize(new Dimension(720, 720));
             grid = new Grid();
         }
+
         @Override
         public void paint(Graphics g) {
-            g.setColor(Color.BLACK);
-            for(int i = 10; i < 710; i += 35) {
-                for(int j = 10; j < 710; j += 35) {
-                    g.setColor(Color.WHITE);
-                    g.fillRect(i, j, 35, 35);
-                    g.setColor(Color.BLACK);
-                    g.drawRect(i, j, 35, 35);
-                }
-            }
-            // g.setColor(Color.RED);
-            // g.drawOval((int)getMousePosition().getX(), (int)getMousePosition().getY(), 5, 5);
-            
+            grid.paint(g, getMousePosition());
         }
+
     }
 
-    class Cell { //cell class
-        //instance variables
-        int xPos;
-        int yPos;
-        Color fillColor;
-        Color strokeColor;
-
-        public Cell() { // deafault constructor
-            xPos = 0;
-            yPos = 0;
-            fillColor = Color.WHITE;
-            strokeColor = Color.BLACK;
-        }
-        public Cell(int x, int y, Color f, Color b) { // paramatarised constructor
-            this.xPos = x;
-            this.yPos = y;
-            this.fillColor = f;
-            this.strokeColor = b;
-        }
+    public static void main(String[] args) throws Exception {
+        Main window = new Main();
+        window.run();
     }
 
-    public class Grid { //Grid class
-        Cell[][] cells = new Cell[20][20]; //a grid is an array of cells
-        
-        public void Grid(Cell[][] cells) {
-            this.cells = cells;
-            for(int i = 0; i < cells.length; i++) {
-                for(int j = 0; j < cells[i].length; j++) {
-                    cells[i][j] = new Cell(10+35*i, 10+35*j, Color.WHITE, Color.BLACK);
-                }
-            }
-        }
-    }
-
-    public Main() {
+    private Main() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Canvas canvas = new Canvas();
+        App canvas = new App();
         this.setContentPane(canvas);
         this.pack();
         this.setVisible(true);
     }
+
     public void run() {
-        while(true) {
+        while (true) {
             this.repaint();
         }
     }
-
-    
-    
 }
