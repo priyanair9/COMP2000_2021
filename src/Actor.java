82ade795910f4fe5647d3e921c3227658a529aaa
priyanair9 <priya.nair@students.mq.edu.au>
2021-08-18 14:16:09 +1000

    updated actor

--- src/Actor.java
+++ src/Actor.java
@@ -4,14 +4,10 @@ import java.util.ArrayList;
 public abstract class Actor {
     Color color;
     Cell loc;
-
     ArrayList<Polygon> polygons = new ArrayList<Polygon>();
 
     public void paint(Graphics g) {
         g.setColor(color);
-      //   g.fillRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
-      //   g.setColor(Color.GRAY);
-      //   g.drawRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
 
         for(Polygon p: polygons) {
            g.drawPolygon(p);
