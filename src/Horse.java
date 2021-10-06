20a5fbf034491f2a2a7e8013598e2797f3fb50dd
priyanair9 <priya.nair@students.mq.edu.au>
2021-10-06 13:46:40 +1100

    Update Horse.java

--- src/Horse.java
+++ src/Horse.java
@@ -1,3 +1,23 @@
-public class Horse {
-  // Task 22
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Horse extends Actor { 
+  public Motif icon;
+
+  public Horse(Cell inLoc, float inRedness) {
+    setLocation(inLoc);
+        redness = inRedness;
+        turns = 1;
+        moves = 1;
+        setPoly();
+  }
+
+  protected void setPoly() {
+    icon = new Motif("images/Chess_tile_nl.png");
+  }
+
+  @Override
+  public void paint(Graphics g) {
+    icon.draw(g, loc.x, loc.y, Color.PINK);
+  }
 }
